224f9466f3b8e1542228415dc84e62e117510789
Pmk456 <45127373+pmk456@users.noreply.github.com>
2021-06-09 16:44:37 +0530

    Version 2.0

--- reset.py
+++ reset.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-import os
-ip_file = ".ip.txt"
-ssh_file = "ssh"
-if ip_file not in os.listdir ( ) and ssh_file not in os.listdir ( ):
-    print ("[*] No Files Detected, So Nothing Deleted!")
-if ip_file in os.listdir():
-    print ("[*] Deleting ip.txt File")
-    os.system ("rm .ip.txt")
-    print("Removed Ip File")
-if ssh_file in os.listdir ( ):
-    print ("[*] Deleting SSH File")
-    os.system ("rm ssh")
-    print("[*] Removed SSH File")
+import os, sys
+
+if not "ssh.bat" and ".ip.dat" and "ssh" not in os.listdir():
+    print("[*] No Files Detected, So Nothing Deleted!")
+    sys.exit(1)
+if "ssh.bat" in os.listdir():
+    os.remove("ssh.bat")
+    print("[+] Removed ssh.bat File")
+if ".ip.dat" in os.listdir():
+    os.remove(".ip.dat")
+    print("[+] Removed Ip File")
+if "ssh" in os.listdir():
+    os.remove("ssh")
+    print("Remove SSH File")
 print("[*] Reset Success")
-print ("[*] Created By Pmk")
+print("[*] Created By Pmk")
