e4e4f0e94214204a6605b2e8554436beb74361ca
Pmk456 <45127373+pmk456@users.noreply.github.com>
2021-06-09 16:41:03 +0530

    Added Support For Windows 10
    Re-Written The Whole Code
    Added Support For Windows 10 
    Added Support For Termux

--- setup.py
+++ setup.py
@@ -1,124 +1,194 @@
-#!/usr/bin/python3
-import os
-from time import sleep
-from sys import exit
-
+import ctypes, os, pickle, sys
+import subprocess as sp
 system = None
 
 
+admin = ctypes.windll.shell32.IsUserAnAdmin() == 1
+
+if sys.platform == 'win32':
+    system = 'Windows'
+elif sys.platform == 'linux':
+    system = 'Linux'
+if os.path.exists("/data/data/com.termux/"):
+    system = 'Termux'
 def banner():
-    os.system( 'clear' )
-    print( GREEN, """
-    ____            _    
+    print("""
+     ____            _    
     |  _ \ _ __ ___ | | __
     | |_) | '_ ` _ \| |/ /
     |  __/| | | | | |   < 
-    |_|   |_| |_| |_|_|\_\ """ )
-    print( GREEN, "\n****************************************************************" )
-    print( GREEN, "\n* Copyright of Patan Musthakheem, 2021                         *" )
-    print( GREEN, "\n* www.github.com/pmk456                                        *" )
-    print( GREEN, "\n* www.youtube.com/hackerx                                      *" )
-    print( GREEN, "\n****************************************************************" )
-   
-
-
-if os.path.exists( "/data/data/com.termux/" ):
-    system = "termux"
-else:
-    system = "linux"
-RED = "\033[1;31m"
-GREEN = "\033[0;32m"
-if system == 'linux':
-    if os.path.exists( "/bin/ssh" ) or os.path.exists( "/usr/bin/ssh" ):
-        pass
-    else:
-        print( RED + "[-] SSH IS NOT INSTALLED IN YOU SYSTEM" )
-        per = input( GREEN + "[+] Install SSH [Y/N]: " )
-        if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
-            os.system( "sudo apt install openssh-client --yes" )
+    |_|   |_| |_| |_|_|\_\ """)
+    print("\n****************************************************************")
+    print("\n* Copyright of Patan Musthakheem, 2021                         *")
+    print("\n* www.github.com/pmk456                                        *")
+    print("\n* www.youtube.com/hackerx                                      *")
+    print("\n****************************************************************")
+if system == 'Termux' or system == "Linux":
+    RED = "\033[1;31m"
+    GREEN = "\x1b[0;32m"
+    if system == 'Linux':
+        if os.path.exists("/bin/ssh") or os.path.exists("/usr/bin/ssh"):
+            pass
         else:
-            banner()
-            exit(12)
-elif system == "termux":
-    if os.path.exists("/data/data/com.termux/files/usr/bin/ssh"):
-        pass
-    else:
-        print( RED + "[-] SSH IS NOT INSTALLED IN YOU SYSTEM" )
-        per = input( GREEN + "[+] Install SSH [Y/N]: " )
-        if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
-            os.system( "pkg install openssh" )
-        else:
-            banner()
-            exit(12)
-banner()
-if '.ip.txt' not in os.listdir():
-    print( '[*] Ip File Not Detected!' )
-    i = input( '[*] Enter Ip: ' )
-    n = input( '[*] Enter UserName: ' )
-    with open( '.ip.txt', 'a' ) as f:
-        f.write( i )
-        f.write( '\n' )
-        f.write( n )
-        print( '[*] Ip And Username Has Been Saved To Ip.txt' )
-        f.close()
-else:
-    print( '[*] Ip File Detected!' )
-po = open( '.ip.txt', 'r' )
-po1 = po.readlines()
-ssh_ip = po1[0]
-ssh_un = po1[1]
-ssh_ip1 = ssh_ip
-if 'ssh' in os.listdir():
-    print( '[*] Bash Script Exists' )
-    print( '[*] Delete Everything In Directory Except Setup.py and compile.sh If You want to reset' )
-else:
-    print( '[*] Generating Bash Script...' )
-    lines = f"""#!/bin/bash
-    IP={ssh_ip}
-    echo "[*] Connecting To {ssh_un}..."
-    sleep 0.5
-    echo "[*] Created By Pmk "
-    if ping -q -c 1 -W 1 $IP >/dev/null; then
-    echo "[*] Connected To {ssh_un}"
-    sleep 1
-    clear
-    ssh {ssh_un}@{ssh_ip}
-    else
-    echo "[*] Cant Connect To {ssh_un} No Reply!"
-    echo "[*] Exiting..."
-    exit
-    fi """
-    os.system( 'touch ssh' )
-    os.system( 'chmod +x ssh' )
-    a_file = open( "ssh", "w" )
-    a_file.writelines( lines )
-    a_file.close()
-    sleep( 1.5 )
-    print( '[*] Generated Bash Script!' )
-sleep( 3 )
-os.system( "clear" )
-per = input( "[+] Create RSA Pair For Password Less Connection [Y/N]: " )
-if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
-    print( '[*] Creating RSA Keypair For Password Less connection' )
-    os.system( 'ssh-keygen -t rsa -b 4096' )
-else:
-    pass
-os.system( "clear" )
-per1 = input( "[+] Copy Public Key To Remote Computer [Y/N]: " )
-if per1 == 'y' or per == 'Y' or per == "yes" or per == "Yes":
-    os.system( "clear" )
-    print( '[*] Copying Public Key ' )
-    print( '[*] It Will Ask You Password Of Remote Computer Enter It!' )
-    os.system( f'ssh-copy-id {ssh_un}@{ssh_ip}' )
-i = input( '[*] Move It To /usr/bin For Accessing It anywhere [Y/N]: ' )
-if i == 'y' or i == 'yes' or i == "Y":
-    print( RED, "[*] This Needs Sudo Permission" )
-    name = input( "[*] Enter Name For Accessing It Anywhere From Terminal: " )
-    if system == "termux":
-        os.system( f"mv ssh /data/data/com.termux/files/usr/bin/{name}" )
-    elif system == "linux":
-        os.system( f"sudo mv ssh /usr/bin/{name}" )
-    else:
-        pass
-else:
+            print(RED + "[-] SSH IS NOT INSTALLED IN YOU SYSTEM")
+            per = input(GREEN + "[+] Install SSH [Y/N]: ")
+            if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
+                os.system("sudo apt install openssh-client --yes")
+            else:
+                banner()
+                sys.exit(12)
+    elif system == "Termux":
+        if not os.path.exists("/data/data/com.termux/files/usr/bin/ssh"):
+            print(RED + "[-] SSH IS NOT INSTALLED IN YOU SYSTEM")
+            per = input(GREEN + "[+] Install SSH [Y/N]: ")
+            if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
+                os.system("pkg install openssh")
+            else:
+                banner()
+                exit(12)
+    print(GREEN)
+    os.system("clear")
     banner()
+    if '.ip.dat' not in os.listdir():
+        print("[+] IP File Not Detected")
+        ip = input("[+] Enter Ip Of Remote Computer: ")
+        un = input("[+] Enter User Name: ")
+        ip_un = [ip, un]
+        with open('.ip.dat', 'wb') as file:
+            try:
+                pickle.dump(ip_un, file)
+            except Exception:
+                print("[-] Can\'t Save IP And User Name")
+            else:
+                print("[+] Save Success!")
+    else:
+        with open(".ip.dat", 'rb') as file:
+            ip_un = pickle.load(file)
+            ssh_ip = ip_un[0]
+            ssh_un = ip_un[1]
+    if 'ssh' in os.listdir():
+        print("[+] Bash Script Exists")
+    else:
+        print('[*] Generating Bash Script...')
+        lines = f"""#!/bin/bash
+        IP={ssh_ip}
+        echo "[+] Connecting To {ssh_un}..."
+        sleep 0.5
+        echo "[+] Created By Pmk "
+        if ping -q -c 1 -W 1 $IP >/dev/null; then
+        echo "[+] Connected To {ssh_un}"
+        sleep 1
+        clear
+        ssh {ssh_un}@{ssh_ip}
+        else
+        echo "[+] Cant Connect To {ssh_un} No Reply!"
+        echo "[+] Exiting..."
+        exit
+        fi """
+        os.system('touch ssh')
+        os.system('chmod +x ssh')
+        a_file = open("ssh", "w")
+        a_file.writelines(lines)
+        a_file.close()
+        sleep(1.2)
+        print('[+] Generated Bash Script!')
+if system == "Windows":
+    os.system("cls")
+    banner()
+    if '.ip.dat' not in os.listdir():
+        print("[+] IP File Not Detected")
+        ip = input("[+] Enter Ip Of Remote Computer: ")
+        un = input("[+] Enter User Name: ")
+        ip_un = [ip, un]
+        with open('.ip.dat', 'wb') as file:
+            try:
+                pickle.dump(ip_un, file)
+            except Exception:
+                print("[-] Can\'t Save IP And User Name")
+            else:
+                print("[+] Save Success!")
+    else:
+        with open(".ip.dat", 'rb') as file:
+            ip_un = pickle.load(file)
+            ssh_ip = ip_un[0]
+            ssh_un = ip_un[1]
+            file.close()
+    if 'script.bat' not in os.listdir():
+        with open(".ip.dat", 'rb') as file:
+            ip_un = pickle.load(file)
+            ssh_ip = ip_un[0]
+            ssh_un = ip_un[1]
+        print('[+] Generating Batch File')
+        script = f"""
+        @echo off
+        echo [+] Connecting To {ssh_un}
+        echo [+] Created By Pmk 
+        ping -n 1 {ssh_ip} | find "TTL=" >nul
+        if errorlevel 1 (
+        echo [+] Cant Connect To {ssh_un} No Reply!
+        echo [+] Exiting...
+        EXIT /B 0
+        ) else (
+        echo [+] Connected To {ssh_un}
+        timeout 1 > nul
+        cls
+        ssh {ssh_un}@{ssh_ip}
+        )
+        """
+        with open("script.bat", 'w') as file:
+            file.writelines(script)
+            file.close()
+if system != "Windows":
+    os.system("clear")
+else:
+    os.system("cls")
+per = input("[+] Create RSA Pair For Password Less Connection [Y/N]: ")
+if per == 'y' or per == 'Y' or per == "yes" or per == "Yes":
+    print('[+] Creating RSA Keypair For Password Less connection')
+    os.system('ssh-keygen -t rsa -b 4096')
+
+if system != 'Windows':
+    os.system("clear")
+    per1 = input("[+] Copy Public Key To Remote Computer [Y/N]: ")
+    if per1 == 'y' or per == 'Y' or per == "yes" or per == "Yes":
+        os.system("clear")
+        print('[+] Copying Public Key ')
+        print('[+] It Will Ask You Password Of Remote Computer Enter It!')
+        os.system(f'ssh-copy-id {ssh_un}@{ssh_ip}')
+    i = input('[+] Move It To /usr/bin For Accessing It anywhere [Y/N]: ')
+    if i == 'y' or i == 'yes' or i == "Y":
+        name = input("[+] Enter Name For Accessing It Anywhere From Terminal: ")
+        if system == "Termux":
+            os.system(f"mv ssh /data/data/com.termux/files/usr/bin/{name}")
+        elif system == "Linux":
+            print(RED, "[+] This Needs Sudo Permission")
+            print(GREEN)
+            os.system(f"sudo mv ssh /usr/bin/{name}")
+        else:
+            pass
+    else:
+        os.system("clear")
+        banner()
+else:
+    os.system('cls')
+    per1 = input("[+] Copy Public Key To Remote Computer [Y/N]: ")
+    if per1 == 'y' or per == 'Y' or per == "yes" or per == "Yes":
+        print("[+] Copying Public Key File To Remote Computer")
+        print('[+] It Will Ask You Password Of Remote Computer Enter It!')
+        cmd = f"""type $env:USERPROFILE\.ssh\id_rsa.pub | ssh {ssh_un}@{ssh_ip} "cat >> .ssh/authorized_keys" """
+        sp.run(["powershell", "-Command", cmd])
+    else:
+        os.system("cls")
+
+if system == "Windows":
+    i = input('[+] Move The Script To C:\Windows\System32 For Accessing It anywhere [Y/N]: ')
+    if i == 'y' or i == 'yes' or i == "Y":
+        print("[+] THIS NEEDS ADMINISTRATOR PERMISSION")
+        name = input("[+] Enter Name For Accessing It Anywhere From cmd (or) Powershell: ")
+        if admin:
+            sp.run(['powershell', '-Command', 'mv', 'script.bat', f'C:\Windows\\System32\\{name}.bat'])
+        else:
+            print("[+] NOW IT WILL ASK FOR ADMINISTRATOR PERMISSION")
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    else:
+        banner()
